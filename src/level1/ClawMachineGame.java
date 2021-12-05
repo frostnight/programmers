7d57641fab5e8af31ba282d852ce0dbfaad49908
frostnight <frostnightsong@gmail.com>
2021-12-05 17:28:40 +0900

    카카오 크레인 인형 뽑기 문제 초기화

---
+++ src/level1/ClawMachineGame.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class ClawMachineGame {
+
+	public static void main(String[] args){
+
+		int[][] board = {};
+		int[] moves = {};
+
+		int answer = new ClawMachineGame().solution(board, moves);
+
+		System.out.println("answer = " + answer);
+	}
+
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		return answer;
+	}
+}
