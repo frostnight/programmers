a823f2d39da228954897d1003d69b7664071f828
frostnight <frostnightsong@gmail.com>
2021-12-10 19:06:54 +0900

    카카오 크레인 인형 뽑기 문제2

--- src/level1/ClawMachineGame.java
+++ src/level1/ClawMachineGame.java
@@ -4,8 +4,8 @@ public class ClawMachineGame {
 
 	public static void main(String[] args){
 
-		int[][] board = {};
-		int[] moves = {};
+		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] moves = {1,5,3,5,1,2,1,4};
 
 		int answer = new ClawMachineGame().solution(board, moves);
 
@@ -14,6 +14,10 @@ public class ClawMachineGame {
 
 	public int solution(int[][] board, int[] moves) {
 		int answer = 0;
+
+		// 해당 바구니에서 꺼내는 메소드
+		//
+
 		return answer;
 	}
 }
