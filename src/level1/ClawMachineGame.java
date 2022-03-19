036baebb951f0a5552327653a0f7976ad45d7bc4
frostnight <frostnightsong@gmail.com>
2022-03-19 18:13:57 +0900

    카카오 크레인 인형 뽑기 문제 클리어

--- src/level1/ClawMachineGame.java
+++ src/level1/ClawMachineGame.java
@@ -1,5 +1,8 @@
 package level1;
 
+
+import java.util.Stack;
+
 public class ClawMachineGame {
 
 	public static void main(String[] args){
@@ -14,10 +17,38 @@ public class ClawMachineGame {
 
 	public int solution(int[][] board, int[] moves) {
 		int answer = 0;
-
-		// 해당 바구니에서 꺼내는 메소드
-		//
-
+		Stack<Integer> bucket = new Stack<>();
+		for(int i=0; i < moves.length; i++){
+			int item = getItem(board, moves[i] - 1);
+			if(item > 0){
+				answer += checkItem(bucket, item);
+			}
+		}
 		return answer;
 	}
+
+	public int getItem(int[][] board, int y){
+		int result = 0;
+		for(int x = 0; x < board.length; x++){
+			if(board[x][y] != 0){
+				result = board[x][y];
+				board[x][y] = 0;
+				break;
+			}
+		}
+		return result;
+	}
+
+	public int checkItem(Stack<Integer> bucket, int item){
+		int remove_count = 0;
+
+		if(bucket.size() > 0 && bucket.peek() == item){
+			bucket.pop();
+			remove_count = 2;
+		} else {
+			bucket.push(item);
+		}
+		return remove_count;
+	}
+
 }
