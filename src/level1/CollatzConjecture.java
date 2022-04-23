243617d56d0eddf4f12e994f0c38c235c1331fbe
frostnight <frostnightsong@gmail.com>
2022-04-23 16:59:16 +0900

    콜라츠 추측1

---
+++ src/level1/CollatzConjecture.java
@@ -0,0 +1,26 @@
+package level1;
+
+/**
+ * 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+ * 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+ * 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다
+ */
+public class CollatzConjecture {
+	public static void main(String[] args) {
+		int num = 0;
+		System.out.println(solution(num));
+	}
+
+	public static int solution(int num) {
+		int answer = 0;
+		return answer;
+	}
+
+	public static int oddCalc(){
+
+	}
+
+	public static int evenCalc(){
+
+	}
+}
